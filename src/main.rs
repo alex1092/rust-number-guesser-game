e5f0dbc86208fed13b997addb4bf1cec26f31a43
Alex Little <alex.little@neds.com>
2024-02-04 14:24:21 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+// 👆 To obtain the users input we need to bring in the io library "input/output"
+// This comes from the standard library (std)
+// check out the prelude here file:///Users/alex/.rustup/toolchains/stable-aarch64-apple-darwin/share/doc/rust/html/std/prelude/index.html
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        // The :: syntax in the ::new line indicates that new is an associated function of the String type.
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
